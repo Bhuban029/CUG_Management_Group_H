067980643637b1d6f16603d605ff06c2df168170
Bhuban029 <134909972+Bhuban029@users.noreply.github.com>
2024-07-09 11:55:38 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -7,14 +7,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBzzRRDsgTDmmiXqxecfJDpQRPPD-bccKw",
-  authDomain: "cug-management-1042a.firebaseapp.com",
-  projectId: "cug-management-1042a",
-  storageBucket: "cug-management-1042a.appspot.com",
-  messagingSenderId: "481415058881",
-  appId: "1:481415058881:web:97aefacd91023a7b6ccb40",
-  measurementId: "G-VEBW245WYH",
-  databaseURL: "https://cug-management-1042a-default-rtdb.firebaseio.com/",
+  apiKey: "AIzaSyB5gQX_cPY1oH-0Au7xwJYCN8daTMjpYYo",
+  authDomain: "cugmanagement-3f4f0.firebaseapp.com",
+  projectId: "cugmanagement-3f4f0",
+  storageBucket: "cugmanagement-3f4f0.appspot.com",
+  messagingSenderId: "145760142049",
+  appId: "1:145760142049:web:7d7d115d7ba4b445202da0",
+  measurementId: "G-T7TZT6GRTT"
 };
 
 // Initialize Firebase
